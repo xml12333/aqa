0ae044e27dd290a4bf87f02a909249b418eb32d3
xml12333 <Mykola.Trafimenkov@sumitec.local>
2019-11-14 10:23:27 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,71 @@
+import site.PageTesting;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static WebDriver driver;
+    public  static WebDriverWait wait;
+
+    String loginEmail = "xml12333@gmail.com";
+    String createAccountContent = "CREATE AN ACCOUNT";
+    String errorMessage= "There are 2 errors";
+
+    static PageTesting pageTesting;
+
+    @BeforeClass
+    public static void setupClass() {
+
+        System.setProperty("webdriver.chrome.driver","D:\\temp\\chromedriver_78\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        pageTesting = new PageTesting(driver);
+
+    }
+
+    @Test
+    public void SignIntoAccountTest()  {
+
+        pageTesting
+                .openApplication()
+                .goToSignInPage()
+                .enterRegistrationEmail(loginEmail)
+                .goToRegistrationPage();
+
+        Assert.assertEquals( createAccountContent, pageTesting.getCreateAccountContentValue());
+
+
+    }
+
+    @Test
+    public void CreateAccountTest(){
+        SignIntoAccountTest();
+        pageTesting
+                .setFirstName("Nik")
+                .setLastName("Nik")
+                .enterPassword("123444")
+                .setAddrFirstName("NikF")
+                .setAddrLastName("NikL")
+                .setAddress("Some str.")
+                .setCity("Kiev")
+                .setCode("124")
+                .setMobilePhone("05012345678")
+                .setAlias("Some str.")
+                .registerAccount();
+
+        Assert.assertEquals( errorMessage, pageTesting.getCreateAccountErrorMessage());
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/site/PageTesting.java
@@ -0,0 +1,134 @@
+package site;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageTesting {
+
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private static final String url = "http://automationpractice.com/index.php";
+    private static final By registerEmail = By.id("email_create");
+    private static final By signInButton = By.cssSelector("#header > div.nav > div > div > nav > div.header_user_info > a");
+    private static final By createAccountButton = By.cssSelector("#SubmitCreate");
+    private static final By registrationForm = By.cssSelector("#account-creation_form");
+    private static final By createAccountContent = By.xpath("//h3[text()='Create an account']");
+
+    private static final By firstName = By.id("customer_firstname");
+    private static final By lastName = By.id("customer_lastname");
+    private static final By email = By.id("email");
+    private static final By password = By.id("passwd");
+    private static final By addressFirstName = By.id("firstname");
+    private static final By addressLastName = By.id("lastname");
+    private static final By address = By.id("address1");
+    private static final By city = By.id("city");
+    private static final By code = By.id("postcode");
+    private static final By mobilePhone = By.id("phone_mobile");
+    private static final By alias = By.id("alias");
+    private static final By registerButton = By.id("submitAccount");
+    private static final By errorMessage = By.xpath("//div[contains(@class,'alert')]/p");
+
+    public PageTesting(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(this.driver, 5);
+    }
+
+
+    public PageTesting setFirstName(String name) {
+        driver.findElement(firstName).clear();
+        driver.findElement(firstName).sendKeys(name);
+        return this;
+    }
+
+    public PageTesting setLastName(String nameLast) {
+        driver.findElement(lastName).sendKeys(nameLast);
+        return this;
+    }
+
+    public PageTesting enterEmail(String accEmail) {
+        driver.findElement(email).sendKeys(accEmail);
+        return this;
+    }
+
+
+    public PageTesting enterPassword(String pwd) {
+        driver.findElement(password).sendKeys(pwd);
+        return this;
+    }
+
+    public PageTesting setAddrFirstName(String name) {
+        driver.findElement(addressFirstName).sendKeys(name);
+        return this;
+    }
+
+    public PageTesting setAddrLastName(String nameLast) {
+        driver.findElement(addressLastName).sendKeys(nameLast);
+        return this;
+    }
+
+
+    public PageTesting setAddress(String addr) {
+        driver.findElement(address).sendKeys(addr);
+        return this;
+    }
+
+    public PageTesting setCity(String cityname) {
+        driver.findElement(city).sendKeys(cityname);
+        return this;
+    }
+
+    public PageTesting setCode(String postCode) {
+        driver.findElement(code).sendKeys(postCode);
+        return this;
+    }
+
+    public PageTesting setMobilePhone(String phone) {
+        driver.findElement(mobilePhone).sendKeys(phone);
+        return this;
+    }
+
+
+    public PageTesting setAlias(String Aladdr) {
+        driver.findElement(alias).sendKeys(Aladdr);
+        return this;
+    }
+
+    public PageTesting registerAccount() {
+        driver.findElement(registerButton).click();
+        return this;
+    }
+
+    public String getCreateAccountErrorMessage() {
+        return driver.findElement(errorMessage).getText();
+    }
+
+    public PageTesting openApplication() {
+        driver.get(url);
+        return this;
+    }
+
+
+    public PageTesting goToSignInPage() {
+        driver.findElement(signInButton).click();
+        return this;
+    }
+
+    public PageTesting enterRegistrationEmail(String email) {
+        driver.findElement(registerEmail).sendKeys(email);
+        return this;
+    }
+
+
+    public PageTesting goToRegistrationPage() {
+        driver.findElement(createAccountButton).click();
+        return this;
+    }
+
+    public String getCreateAccountContentValue() {
+        return driver.findElement(createAccountContent).getText();
+    }
+
+}
